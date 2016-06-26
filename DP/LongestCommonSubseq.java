3f954c72f1d463b13bea0e8f653e2375a799faf9
sherrychen326 <sherrychen326@users.noreply.github.com>
2016-06-26 15:55:53 +0800

    Update LongestCommonSubseq.java

--- DP/LongestCommonSubseq.java
+++ DP/LongestCommonSubseq.java
@@ -1,4 +1,5 @@
 package dynamic_programming;
+//find the longest common subsequence of two input strings
 
 
 public class LongestCommonSubseq {  
@@ -13,79 +14,7 @@ public class LongestCommonSubseq {
      * @param string 
      * @param string2 
      */  
-    public static void findLCS(char[] string ,char[] string2) {  
-        int m = string.length;  
-        int n = string2.length;  
-        int[][] c = new int[m+1][n+1];  
-        int[][] b = new int[m+1][n+1];  
-        for(int i=0;i<m;i++) {  
-            for(int j=0;j<n;j++) {  
-                if(string[i]==string2[j]) {  
-                    c[i+1][j+1]=c[i][j]+1;  
-                    b[i+1][j+1]= '\\';  
-                } else {  
-                    if(c[i][j+1] >= c[i+1][j]) {  
-                        c[i+1][j+1] = c[i][j+1];  
-                        b[i+1][j+1]= '|';  
-                    } else {  
-                        c[i+1][j+1] = c[i+1][j];  
-                        b[i+1][j+1]= '-';  
-  
-                    }  
-                }  
-            }  
-        }  
-          
-        System.out.println("LCS长度:"+c[m][n]);  
-          
-        System.out.println("LCS序列:");  
-        printLCS(b,string,m,n);  
-    }  
-    
-    public static void findLCS2(char[] str1, char[] str2) {
-    	int len1 = str1.length;
-    	int len2 = str2.length;
-    	int[][] cMatrix = new int[len1+1][len2+1];
-    	int[][] bMatrix = new int[len1+1][len2+1];
-    	//initialize cMatrix and bMatrix
 
-    	for(int i=1;i<=len1;i++){
-    		for(int j=1;j<=len2;j++){
-    			if(str1[i-1]==str2[j-1]){
-    				cMatrix[i][j]=cMatrix[i-1][j-1]+1;
-    				bMatrix[i][j]='\\';
-    			}else{
-    				if(cMatrix[i-1][j]>=cMatrix[i][j-1]){
-    					cMatrix[i][j]=cMatrix[i-1][j];
-    					bMatrix[i][j]='|';
-    				} else if(cMatrix[i][j-1]>cMatrix[i-1][j]){
-    					cMatrix[i][j]=cMatrix[i][j-1];
-    					bMatrix[i][j]='-';
-    				}
-    			}
-    		}
-    	}
-    	 System.out.println("LCS长度:"+cMatrix[len1][len2]);  
-         
-         System.out.println("LCS序列:");  
-         printLCS(bMatrix,str1,len1,len2);
-    }
-    
-    private static void printLCS(int[][] b, char[] string, int i, int j) {  
-        if(i==0 || j==0) {  
-            return ;  
-        }  
-          
-        if(b[i][j]=='\\') {  
-            printLCS(b, string, i-1, j-1);  
-            System.out.println(string[i-1]);  
-        } else if(b[i][j]=='|'){  
-            printLCS(b, string, i-1, j);  
-        } else {  
-            printLCS(b, string, i, j-1);  
-        }  
-           
-    } 
     public static void findLCS1(char[] string1, char[] string2) {  
         int m = string1.length;  
         int n = string2.length;  
@@ -116,5 +45,50 @@ public class LongestCommonSubseq {
         }  
     }  
     
-   
+             
+    
+    public static void findLCS2(char[] str1, char[] str2) {
+    	int len1 = str1.length;
+    	int len2 = str2.length;
+    	int[][] cMatrix = new int[len1+1][len2+1];
+    	int[][] bMatrix = new int[len1+1][len2+1];
+    	//initialize cMatrix and bMatrix
+
+    	for(int i=1;i<=len1;i++){
+    		for(int j=1;j<=len2;j++){
+    			if(str1[i-1]==str2[j-1]){
+    				cMatrix[i][j]=cMatrix[i-1][j-1]+1;
+    				bMatrix[i][j]='\\';
+    			}else{
+    				if(cMatrix[i-1][j]>=cMatrix[i][j-1]){
+    					cMatrix[i][j]=cMatrix[i-1][j];
+    					bMatrix[i][j]='|';
+    				} else if(cMatrix[i][j-1]>cMatrix[i-1][j]){
+    					cMatrix[i][j]=cMatrix[i][j-1];
+    					bMatrix[i][j]='-';
+    				}
+    			}
+    		}
+    	}
+    	 System.out.println("LCS length:"+cMatrix[len1][len2]);  
+         
+         System.out.println("LCS display:");  
+         printLCS(bMatrix,str1,len1,len2);
+    }
+        
+    private static void printLCS(int[][] b, char[] string, int i, int j) {  
+        if(i==0 || j==0) {  
+            return ;  
+        }  
+          
+        if(b[i][j]=='\\') {  
+            printLCS(b, string, i-1, j-1);  
+            System.out.println(string[i-1]);  
+        } else if(b[i][j]=='|'){  
+            printLCS(b, string, i-1, j);  
+        } else {  
+            printLCS(b, string, i, j-1);  
+        }  
+           
+    }
 }    
